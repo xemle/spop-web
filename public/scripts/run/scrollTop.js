c6a68c9a437f65cf39e6618ac5d8e075bb506c12
Xemle <xemle@phtagr.org>
2015-06-14 15:23:43 +0200

    Scroll to top on route change

---
+++ public/scripts/run/scrollTop.js
@@ -0,0 +1,13 @@
+"use strict";
+
+angular
+  .module('app')
+  .run([
+    '$rootScope',
+    '$anchorScroll',
+    function($rootScope, $anchorScroll) {
+      $rootScope.$on('$routeChangeSuccess', function() {
+        $anchorScroll('main');
+      });
+    }
+  ]);
