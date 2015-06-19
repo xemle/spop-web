7f898939beadd16756b286889dbfafad471c7bbc
Xemle <xemle@phtagr.org>
2015-06-19 11:01:34 +0200

    Build stylesheet from less

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,10 @@ var gulp = require('gulp'),
     minifyCss = require('gulp-minify-css'),
     minifyHtml = require('gulp-minify-html'),
     templateCache = require('gulp-angular-templatecache'),
-    inject = require('gulp-inject');
+    inject = require('gulp-inject'),
+    debug = require('gulp-debug'),
+    less = require('gulp-less'),
+    gutil = require('gulp-util');
 
 
 gulp.task('ng-templates', function() {
@@ -15,7 +18,31 @@ gulp.task('ng-templates', function() {
     .pipe(gulp.dest('.tmp'));
 });
 
-gulp.task('html', ['ng-templates'], function() {
+gulp.task('copyFontsFromBower', function() {
+  var bowerBase = 'public/bower_components/';
+
+  return gulp.src([
+    bowerBase + 'fontawesome/fonts/*',
+    bowerBase + 'bootstrap/fonts/*'
+  ])
+    .pipe(gulp.dest('public/fonts'));
+});
+
+gulp.task('copyFontsToDist', ['copyFontsFromBower'], function() {
+  return gulp.src('public/fonts*')
+    .pipe(gulp.dest('public/fonts'));
+});
+
+gulp.task('less', function() {
+  gulp
+    .src('public/styles/app.less') // This was the line that needed fixing
+    .pipe(less({
+      paths: ['public/styles']
+    }).on('error', gutil.log))
+    .pipe(gulp.dest('public/styles'));
+});
+
+gulp.task('html', ['less', 'ng-templates'], function() {
   var assets = useref.assets(),
       sources = gulp.src('.tmp/templates.js');
 
@@ -26,8 +53,15 @@ gulp.task('html', ['ng-templates'], function() {
     .pipe(gulpif('*.css', minifyCss()))
     .pipe(assets.restore())
     .pipe(useref())
-    .pipe(gulp.dest('dist'))
+    .pipe(gulp.dest('dist'));
 });
 
+gulp.task('dev', ['copyFontsFromBower', 'less']);
 
-gulp.task('default', ['html']);
+gulp.task('watch', ['dev'], function() {
+  gulp.watch('public/styles/*.less', ['less']);
+});
+
+gulp.task('dist', ['dev', 'html']);
+
+gulp.task('default', ['dev']);
