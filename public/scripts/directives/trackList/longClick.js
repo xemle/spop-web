8b6184d3f466603b1a0f1ca1c62ef7a036104b4c
Xemle <xemle@phtagr.org>
2015-06-19 22:47:15 +0200

    Fix longClick directive

--- public/scripts/directives/trackList/longClick.js
+++ public/scripts/directives/trackList/longClick.js
@@ -3,31 +3,88 @@
 angular
   .module('app')
   .directive('longClick', [
-    function() {
+    '$timeout',
+    '$parse',
+    function($timeout, $parse) {
       return {
-        scope: {
-          longClick: '&'
-        },
-        link: function(scope, $element) {
-          var INTERVAL = 500,
-              pressed;
+        link: function(scope, $element, $attrs) {
+          var WAIT_INTERVAL = 500,
+              MAX_MOVE_DELTA = 10,
+              longClickExp = $parse($attrs.longClick),
+              startPoint,
+              timer;
 
-          function down() {
-            pressed = +new Date();
+          // Borrowed from ng-touch: https://github.com/angular/angular.js/blob/master/src/ngTouch/swipe.js getCoordinates()
+          function getPoint($event) {
+            var originalEvent = $event.originalEvent || $event;
+            var touches = originalEvent.touches && originalEvent.touches.length ? originalEvent.touches : [originalEvent];
+            var e = (originalEvent.changedTouches && originalEvent.changedTouches[0]) || touches[0];
+
+            return {
+              x: e.clientX,
+              y: e.clientY
+            };
           }
 
-          function up() {
-            var now = +new Date(),
-                elapsed = now - pressed;
-            if (elapsed > INTERVAL) {
-              scope.longClick();
+          function start($event) {
+            if (timer) {
+              return eventBlackhole($event);
             }
+            startPoint = getPoint($event);
+            timer = $timeout(function() {
+              longClickExp(scope);
+              timer = false;
+            }, WAIT_INTERVAL);
+            return eventBlackhole($event);
           }
 
-          $element.on('mousedown', down);
-          $element.on('mouseup', up);
-          $element.on('touchstart', down);
-          $element.on('touchend', up);
+          function cancel($event) {
+            $timeout.cancel(timer);
+            timer = false;
+            return eventBlackhole($event);
+          }
+
+          /**
+           * Calculates distance from start and cancel on maximum distance
+           *
+           * @param {event} $event
+           */
+          function move($event){
+            if (!timer) {
+              return eventBlackhole($event);
+            }
+
+            var point = getPoint($event),
+                deltaX = startPoint.x - point.x,
+                deltaY = startPoint.y - point.y,
+                delta = Math.sqrt(deltaX * deltaX + deltaY * deltaY);
+
+            if (delta > MAX_MOVE_DELTA) {
+              return cancel($event);
+            }
+            return eventBlackhole($event);
+          }
+
+          // http://stackoverflow.com/questions/3413683/disabling-the-context-menu-on-long-taps-on-android
+          function eventBlackhole($event) {
+            if (!$event) {
+              return false;
+            }
+            $event.preventDefault && $event.preventDefault();
+            $event.stopPropagation && $event.stopPropagation();
+            $event.cancelBubble = true;
+            $event.returnValue = false;
+            return false;
+          }
+
+          $element.on('mousedown', start);
+          $element.on('mousemove', move);
+          $element.on('mouseleave', cancel);
+          $element.on('mouseup', cancel);
+
+          $element.on('touchstart', start);
+          $element.on('touchmove', move);
+          $element.on('touchend', cancel);
         }
       };
     }
