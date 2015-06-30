8e96929844e466bfd74be75ac45e05d37c956bdb
Xemle <xemle@phtagr.org>
2015-06-30 21:31:55 +0200

    Add add track menu item

--- public/scripts/modules/track/trackList.js
+++ public/scripts/modules/track/trackList.js
@@ -10,21 +10,23 @@ angular
     function($rootScope, $location, StatusService, QueueService) {
       return {
         scope: {
-          trackList: '=',
+          trackList: '='
         },
         templateUrl: 'scripts/modules/track/trackList.html',
         link: function(scope) {
 
           scope.items = [
             { icon: 'fa-play', action: 'play', text: 'Play' },
+            { icon: 'fa-plus', action: 'add', text: 'Add track' },
             { icon: 'fa-music', action: 'title', text: 'Search title' },
             { icon: 'fa-user', action: 'artist', text: 'Search artist' },
-            { icon: 'fa-play-circle', action: 'album', text: 'Search album' },
-            { icon: 'fa-trash-o', action: 'remove', text: 'Remove' }
+            { icon: 'fa-play-circle', action: 'album', text: 'Search album' }
           ];
           scope.itemClick = function(track, item) {
             if (item.action === 'play') {
               QueueService.goto(track);
+            } else if (item.action === 'add') {
+              QueueService.addTrack(track);
             } else if (item.action === 'remove') {
               QueueService.removeTrack(track);
             } else if (item.action === 'title') {
